3edc15899d89f4abfe532b378dcc071d1575cf11
athif <athifirshad@hotmail.com>
2024-02-09 21:28:05 +0530

    Refactor health record struct and update .gitignore

--- internal/data/healthrecord.go
+++ internal/data/healthrecord.go
@@ -6,27 +6,14 @@ import "time"
 // including personal information, medical history, and treatment plans.
 type PatientHealthRecord struct {
 	ID                   int64     `json:"id"`
-	Name                 string    `json:"name"`
-	DateOfBirth          time.Time `json:"date_of_birth"`
-	Gender               string    `json:"gender"`
-	Address              string    `json:"address"`
-	PhoneNumber          string    `json:"phone_number"`
-	Email                string    `json:"email"`
-	MaritalStatus        string    `json:"marital_status"`
-	Nationality          string    `json:"nationality"`
-	LanguagePreference   string    `json:"language_preference"`
-	TreatmentHistory     []string  `json:"treatment_history"`
-	MedicalDirectives    []string  `json:"medical_directives"`
-	VaccinationHistory   []string  `json:"vaccination_history"`
-	Allergies            []string  `json:"allergies"`
-	FamilyMedicalHistory []string  `json:"family_medical_history"`
-	SocialHistory        []string  `json:"social_history"`
-	ReviewOfSystems      []string  `json:"review_of_systems"`
-	PhysicalExaminations []string  `json:"physical_examinations"`
-	Diagnoses            []string  `json:"diagnoses"`
-	Procedures           []string  `json:"procedures"`
-	PlansAndOrders       []string  `json:"plans_and_orders"`
-	Notes                []string  `json:"notes"`
+	TreatmentHistory     string    `json:"treatment_history"`
+	MedicalDirectives    string    `json:"medical_directives"`
+	VaccinationHistory   string    `json:"vaccination_history"`
+	Allergies            string    `json:"allergies"`
+	FamilyMedicalHistory string    `json:"family_medical_history"`
+	SocialHistory        string    `json:"social_history"`
+	ReviewOfSystems      string    `json:"review_of_systems"`
+	PhysicalExaminations string    `json:"physical_examinations"`
 	CreatedAt            time.Time `json:"created_at"`
 	UpdatedAt            time.Time `json:"updated_at"`
 }
