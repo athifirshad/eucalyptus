7d1d4885daf9abe32a6bc1834d5459ee35171466
athif <athifirshad@hotmail.com>
2024-02-18 17:55:53 +0530

    Refactor getDoctorHandler function to handle error cases and improve error handling

--- cmd/api/doctors.go
+++ cmd/api/doctors.go
@@ -1,23 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/jackc/pgx/v5"
+)
 
 func (app *application) getDoctorHandler(w http.ResponseWriter, r *http.Request) {
 	doctorID, err := app.readIDParam(r)
 	if err != nil {
-		app.badRequestResponse(w, r, err)
+		app.writeJSON(w, http.StatusBadRequest, envelope{"error": err.Error()}, nil)
 		return
 	}
 
 	doctor, err := app.sqlc.GetDoctorById(r.Context(), int32(doctorID))
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		if errors.Is(err, pgx.ErrNoRows) {
+			app.writeJSON(w, http.StatusNotFound, envelope{"error": "doctor not found"}, nil)
+			return
+		}
+		app.writeJSON(w, http.StatusInternalServerError, envelope{"error": "internal server error"}, nil)
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"doctor": doctor}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-	}
-
+	app.writeJSON(w, http.StatusOK, envelope{"doctor": doctor}, nil)
 }
