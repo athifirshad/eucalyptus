3dd2671533c3aaef9e5c3f111d2388f1b7484d42
athif <athifirshad@hotmail.com>
2024-03-08 20:44:09 +0530

    Add CORS middleware to allow cross-origin requests

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/go-chi/cors"
 )
 
 func (app *application) Routes() {
+	app.router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Allows all origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300, 
+	}))
+
 	app.router.Get("/", app.rootHandler)
 	app.router.Get("/status", app.status)
 	app.router.Get("/doctors/{id}", app.getDoctorHandler)
 	app.router.Get("/HealthRecord/{id}", app.getHealthRecordByRecordIdHandler)
 	app.router.Get("/GetHospitalByHospitalId/{id}", app.getHospitalByHospitalIdHandler)
 	app.router.Post("/users", app.registerUserHandler)
-	app.router.Put("/users/activated",app.activateUserHandler)
+	app.router.Put("/users/activated", app.activateUserHandler)
 	app.router.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("404 Page Not Found"))
